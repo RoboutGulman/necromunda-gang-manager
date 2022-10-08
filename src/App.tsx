d07fc864d4b450765869ecb708de305ff0b9a58c
Alex <AlexeyZ213@yandex.ru>
2022-10-08 22:08:34 +0300

    app bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography
+} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Box sx={{
+      flexGrow: 1
+    }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{
+            mr: 2
+          }}>
+          <MenuIcon/>
+        </IconButton>
+        <Typography variant="h6" sx={{
+            flexGrow: 1
+          }}>
+          Necromunda Gang Manager
+        </Typography>
+        <Box mr={3}>
+          <Button color="inherit" variant="outlined">
+            Log In
+          </Button>
+        </Box>
+        <Button color="secondary" variant="contained">
+          Sign Up
+        </Button>
+      </Toolbar>
+    </AppBar>
+  </Box>);
 }
 
 export default App;
