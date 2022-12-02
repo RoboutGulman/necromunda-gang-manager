c62ca5b3d92533867e35b648c13e3e979d4f4dd2
Alex <AlexeyZ213@yandex.ru>
2022-12-02 22:27:28 +0300

    stats table rework

--- src/components/FighterCard/StatsTable.tsx
+++ src/components/FighterCard/StatsTable.tsx
@@ -1,45 +1,54 @@
-import { Box, Divider, ListItem, Stack, styled } from "@mui/material";
-
-const Item = styled(Box)(({ theme }) => ({
-  backgroundColor: "transparent",
-  ...theme.typography.body1,
-  width: "100%",
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import {
+  Box,
+  ListItem,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 interface StatsTableProps {
   stats: { name: string; value: string }[];
 }
 
+const CellWithNoBorder = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    fontWeight: "600",
+    borderColor: theme.palette.secondary.light,
+  },
+  [`&.${tableCellClasses.body}`]: { borderWidth: 0 },
+}));
+
 const StatsTable = (props: StatsTableProps) => {
   return (
-    <>
-      <ListItem disablePadding>
-        <Stack
-          direction="row"
-          sx={{ width: "100%" }}
-          justifyContent="space-around ">
-          {props.stats.map((stat, index) => (
-            <Item key={index}>{stat.name}</Item>
-          ))}
-        </Stack>
-      </ListItem>
-      <Divider
-        sx={{ bgcolor: "secondary.light" }}
-      />
-      <ListItem disablePadding>
-        <Stack
-          direction="row"
-          sx={{ width: "100%" }}
-          justifyContent="space-around ">
-          {props.stats.map((stat, index) => (
-            <Item key={index}>{stat.value}</Item>
-          ))}
-        </Stack>
-      </ListItem>
-    </>
+    <ListItem disablePadding>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow sx={{ borderColor: "secondary.light" }}>
+              {props.stats.map((stat, index) => (
+                <CellWithNoBorder key={index} align="center">
+                  {stat.name}
+                </CellWithNoBorder>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              {props.stats.map((stat, index) => (
+                <CellWithNoBorder key={index} align="center">
+                  {stat.value}
+                </CellWithNoBorder>
+              ))}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </ListItem>
   );
 };
 
