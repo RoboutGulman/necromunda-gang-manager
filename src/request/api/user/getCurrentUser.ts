7c7e7f89f496b1d7013d8c9c7eb7b70738da6b05
RoboutGulman <AlexeyZ213@yandex.ru>
2023-02-01 17:38:53 +0300

    пофиксил баг с запросом getCurrentUser

--- src/request/api/user/getCurrentUser.ts
+++ src/request/api/user/getCurrentUser.ts
@@ -1,11 +1,11 @@
-import {ApiRequest, RequestMethod} from "../../request";
+import { ApiRequest, RequestMethod } from "../../request";
 
 const URL = "/api/user/current";
 
 export type AuthorizeResult = {
   authorized: boolean;
-  user?: {id: number, name: string, registrationDate: string};
-}
+  user?: { id: number; name: string; registrationDate: string };
+};
 
 export async function getCurrentUser(): Promise<AuthorizeResult> {
   const apiRequest = new ApiRequest();
@@ -15,11 +15,14 @@ export async function getCurrentUser(): Promise<AuthorizeResult> {
   const response: any = await apiRequest.send();
 
   return {
-    authorized: response.status === 200,
-    user: response.status === 200 ? {
-      id: response.data["id"],
-      name: response.data["username"],
-      registrationDate: response.data["registration_date"]["date"],
-    } : undefined,
+    authorized: response.data["authorized"],
+    user:
+      response.data["authorized"] === true
+        ? {
+            id: response.data["id"],
+            name: response.data["username"],
+            registrationDate: response.data["registration_date"]["date"],
+          }
+        : undefined,
   };
 }
